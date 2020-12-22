a9780e8f9f98c4b7cb106b042afedf2e72b02fda
tanay-pingalkar <tpingalkar@gmail.com>
2020-12-22 12:06:56 +0500

    added some more things to user schema
    added some more things to user schema

--- user.js
+++ user.js
@@ -4,7 +4,9 @@ const mongoose= require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     userName: String,
-    userPassword: String
+    userPassword: String,
+    roomsJoined: Array,
+    prevRoom: String,
 });
 
 module.exports=mongoose.model('userImformation', UserSchema);
