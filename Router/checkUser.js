5d122131ab3808a60b27ab93d678895f07036332
tanay-pingalkar <tpingalkar@gmail.com>
2021-01-03 14:59:40 +0500

    ok

--- Router/checkUser.js
+++ Router/checkUser.js
@@ -5,11 +5,6 @@ const User= require("./../db/user.js");
 
 
 route.post('/', (req, res) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     User.find({userName:req.body.userName, userPassword:req.body.userPassword},(err,data)=>{
         if(data.length===0){
             res.send('error');
