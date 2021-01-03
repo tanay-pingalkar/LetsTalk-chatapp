529c79ac70b7f145c7c26eb2c25aacf92820b090
tanay-pingalkar <tpingalkar@gmail.com>
2021-01-03 15:38:43 +0500

    lol

--- Router/addRoom.js
+++ Router/addRoom.js
@@ -3,11 +3,10 @@ const route=express.Router()
 const Rooms= require("./../db/rooms.js");
 const User= require("./../db/user.js");
 
-route.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'https://5ff18f9bdc7e5897b7feb5a2--wowchatapp.netlify.app/');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+route.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"https://5ff18f9bdc7e5897b7feb5a2--wowchatapp.netlify.app/");
+    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
 route.post('/:id',(req , res)=>{
--- Router/checkUser.js
+++ Router/checkUser.js
@@ -2,7 +2,12 @@ const express=require('express');
 const route=express.Router()
 const User= require("./../db/user.js");
 
-
+route.use((req,res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin',"https://5ff18f9bdc7e5897b7feb5a2--wowchatapp.netlify.app/");
+    res.setHeader('Access-Control-Allow-Headers',"*");
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 route.post('/', (req, res) =>{
     User.find({userName:req.body.userName, userPassword:req.body.userPassword},(err,data)=>{
